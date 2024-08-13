fe71518434b68ae42cc349d02645b770456ad28a
pohaval <dminnikk@yandex.ru>
2024-08-13 19:38:04 +0700

    edit

--- middleware/vpn.js
+++ middleware/vpn.js
@@ -64,15 +64,17 @@ async function createNewClient(name) {
 
 async function checkOnline() {
   const config = await Wg.show('wg0');
-  return config.wg0.peers.filter(([_, {latestHandshake}]) => latestHandshake > 0).map(([key, peer]) => {
-    const { name, latestHandshake, persistentKeepalive } = peer;
-    const date = new Date(latestHandshake * 1000);
-    console.log(peer)
-    return `${name || key}
-${format(date, 'dd.MMMM.yyyy HH:mm')},
+  console.log(config);
+  return 'test';
+//   return config.wg0.peers.filter(([_, {latestHandshake}]) => latestHandshake > 0).map(([key, peer]) => {
+//     const { name, latestHandshake, persistentKeepalive } = peer;
+//     const date = new Date(latestHandshake * 1000);
+//     console.log(peer)
+//     return `${name || key}
+// ${format(date, 'dd.MMMM.yyyy HH:mm')},
 
-`;
-  }).join('');
+// `;
+//   }).join('');
 
   // const files = fs.readdirSync('/root').filter((name) => name.includes('.conf'));
   // const promises = files.map(async (name) => {
