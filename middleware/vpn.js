632959bbe0889476e262a4da1c762b103ffab19f
pohaval <dminnikk@yandex.ru>
2024-08-13 18:56:52 +0700

    edit

--- middleware/vpn.js
+++ middleware/vpn.js
@@ -63,7 +63,7 @@ async function createNewClient(name) {
 
 async function checkOnline() {
   const config = await Wg.show();
-  console.log(config);
+  console.log(Object.keys(config));
   return config;
 
   // const files = fs.readdirSync('/root').filter((name) => name.includes('.conf'));
