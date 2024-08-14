1f846fc9f0bfa6d32941359517033a1ef3f7797d
pohaval <dminnikk@yandex.ru>
2024-08-14 16:48:14 +0700

    fiux

--- telegram/comands.js
+++ telegram/comands.js
@@ -44,7 +44,7 @@ const create = async (ctx) => {
     user.save();
     ctx.replyWithDocument(new InputFile(path));
   }
-  return ctx?.message?.from || ctx?.update?.callback_query?.from
+  return ctx
 };
 
 const onlineCheck = async (ctx) => {
