0fafa4e6c7acca3cdee1bec8a4ef20e39f518370
pohaval <dminnikk@yandex.ru>
2025-02-05 22:12:47 +0700

    fix

--- middleware/onlineInfo.js
+++ middleware/onlineInfo.js
@@ -18,20 +18,20 @@ function getTotalTransfer(transfer, lastTransfer, lastTotal) {
 async function getInfo() {
   const currentDate = new Date();
   const data = await vpn.wgShow();
-  const filtered = data.filter(({ peer }) => {
-    const aFiveMinuteAgo = new Date(Date.now() - 1000 * 60 * 5);
-    return isWithinInterval(new Date(peer.latestHandshake * 1000), {
-      start: aFiveMinuteAgo,
-      end: currentDate,
-    })
-  });
+  // const filtered = data.filter(({ peer }) => {
+  //   const aFiveMinuteAgo = new Date(Date.now() - 1000 * 60 * 5);
+  //   return isWithinInterval(new Date(peer.latestHandshake * 1000), {
+  //     start: aFiveMinuteAgo,
+  //     end: currentDate,
+  //   })
+  // });
   const lastInfo = await onlineInfo.findOne({}, {}, { sort: { 'created_at' : -1 } })
   const lastTotalRx = lastInfo?.totalRx || 0;
   const lastTotalTx = lastInfo?.totalTx || 0;
   const lastTransferRx = lastInfo?.transferRx || 0;
   const lastTransferTx = lastInfo?.transferTx || 0;
 
-  const promises = filtered.map(async (i) => {
+  const promises = data.map(async (i) => {
     const user = await UserTelegram.findOne({ history: { "$in" : [i.key]} });
     return {
       ...i,
@@ -83,7 +83,7 @@ async function getInfo() {
 
   const res = {
     date: currentDate,
-    count: filtered.length,
+    count: data.length,
     transferTx: resData.transferTx,
     transferRx: resData.transferRx,
     totalTX,
